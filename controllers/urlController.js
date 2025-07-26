7669a8afcb9e7dbed1cc10b19b96c0d6ef1bcbe1
Noam-Sin <noamsinay9907@gmail.com>
2025-07-26 21:33:48 +0300

    fix: fixed errors with mongodb connection

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,4 +1,4 @@
-const Url = require('../models.url');
+const Url = require('../models/url');
 
 exports.addUrl = async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const app = express();
@@ -9,12 +10,12 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/securityDB', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.error("MongoDB connection error:", err));
+.then(() => console.log("Connected to MongoDB Atlas"))
+.catch((err) => console.error("MongoDB connection error:", err));
 
 const urlRoutes = require('./routes/urls');
 app.use('/api/urls', urlRoutes);
