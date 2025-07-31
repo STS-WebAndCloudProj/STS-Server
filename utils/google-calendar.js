a81b61653c7527376b0677e437240888a024baf8
orelklodintwito <noamsinay9907@gmail.com>
2025-07-31 23:24:05 +0300

    fix: google calendar api connection from env

--- utils/google-calendar.js
+++ utils/google-calendar.js
@@ -2,9 +2,9 @@ const { google } = require('googleapis');
 
 
 const oauth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
 );
 
 // 1. Generate Google OAuth URL
